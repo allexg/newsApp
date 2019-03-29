1d18cd4e7ff03698c244825c1fe008c52b4eac50
anagabrielabarbieru94 <anagabrielabarbieru@gmail.com>
2019-03-29 23:44:18 +0200

    Hide api keys

--- flask_impl/aws_translate.py
+++ flask_impl/aws_translate.py
@@ -1,8 +1,8 @@
 import boto3
 
 translate = boto3.client(service_name='translate', region_name='eu-central-1', use_ssl=True,
-                    aws_access_key_id="AKIATIJQG3LSDOFM2UML",
-                    aws_secret_access_key="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")
+                    aws_access_key_id="***************",
+                    aws_secret_access_key="*****************")
 
 result = translate.translate_text(Text="Hello, World",
             SourceLanguageCode="en", TargetLanguageCode="de")
