1eb1bdd3835513cee13f527edc34536c60627db1
anagabrielabarbieru94 <anagabrielabarbieru@gmail.com>
2019-04-02 00:04:32 +0300

    Update translation component

--- flask_impl/application.py
+++ flask_impl/application.py
@@ -32,23 +32,6 @@ async def webSocketConnection(websocket, path):
 
 	actualNews = get_news(category,language,country)
 	await websocket.send(actualNews)
-# deal with webSocketS Connection Server-Side
-
-
-# header_text = '''
-#     <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
-# instructions = '''
-#     <p><em>Hint</em>: This is a RESTful web service! Append a username
-#     to the URL (for example: <code>/Thelonious</code> or  <code>/news</code>) to say hello to
-#     someone specific.</p>\n'''
-# home_link = '<p><a href="/">Back</a></p>\n'
-# footer_text = '</body>\n</html>'
-# application = Flask(__name__)
-# application.add_url_rule('/', 'index', (lambda: header_text +
-#     say_hello() + instructions + footer_text))
-# application.add_url_rule('/news', 'news', view_func=get_news)
-# application.add_url_rule('/<username>', 'hello', (lambda username:
-#     header_text + say_hello(username) + home_link + footer_text))
 
 
 
